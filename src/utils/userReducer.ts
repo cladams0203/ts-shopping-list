0e287ce2d07cfe1f3f71951d889037557085ab59
cladams0203 <cladams0203@gmail.com>
2020-10-02 23:05:58 -0400

    finished initial user reducer

---
+++ src/utils/userReducer.ts
@@ -0,0 +1,44 @@
+type Actions =
+  | { type: "LOADING" }
+  | { type: "ERROR"; payload: string }
+  | {
+      type: "SET_USER";
+      payload: {
+        username: string;
+        email: string;
+        id: number;
+      };
+    };
+
+type State = {
+  user: {};
+  error: string;
+  loading: boolean;
+};
+const initialState: State = {
+  user: {},
+  error: "",
+  loading: false,
+};
+
+export const userReducer = (state = initialState, action: Actions): State => {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "ERROR":
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
